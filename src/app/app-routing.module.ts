61d70251b1705a70c3b58e3c41143144f2b437c6
Marcel <numax.cz@protonmail.ch>
2024-04-03 20:35:21 +0200

    Přidán register/login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./user/login/login.component";
+import {RegisterComponent} from "./user/register/register.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'u',
+    loadChildren: () => import('./user/user.module').then( m => m.UserPageModule)
+  },
+];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
